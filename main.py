905015be42af7a2940f3c9bf770b3a0a95c9991a
Virendra Meena <virenm.meena@gmail.com>
2023-12-18 12:33:47 +0530

    correct proto files and fixed errors. working code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import grpc
 from concurrent import futures
-from service.protos.subscription_pb2_grpc import add_SubscriptionServiceServicer_to_server
+from service.protos import subscription_pb2_grpc
 from service.subscription_service import SubscriptionServicer
 import logging
 
@@ -8,7 +8,7 @@ def serve():
     logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    add_SubscriptionServiceServicer_to_server(SubscriptionServicer(), server)
+    subscription_pb2_grpc.add_SubscriptionServiceServicer_to_server(SubscriptionServicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     print("gRPC server started on port 50051")
